446c9bdcc2242a8100329a8948937fa833fef6ba
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2025-01-08 10:52:23 +0530

    transfer sol to turbine wallet

---
+++ airdrop/transfer.ts
@@ -0,0 +1,38 @@
+import {
+  Transaction,
+  SystemProgram,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  sendAndConfirmTransaction,
+  PublicKey,
+} from "@solana/web3.js";
+import wallet from "./dev-wallet.json";
+const fromWallet = Keypair.fromSecretKey(new Uint8Array(wallet));
+const toWallet = new PublicKey("gyQyxHt1XNvaUJcySyhtv9bRd522HsrXdiyYjeZnAYW");
+const connection = new Connection("https://api.devnet.solana.com");
+(async () => {
+  try {
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: fromWallet.publicKey,
+        toPubkey: toWallet,
+        lamports: (2 - 0.01001) * LAMPORTS_PER_SOL,
+      })
+    );
+    transaction.recentBlockhash = (
+      await connection.getLatestBlockhash("confirmed")
+    ).blockhash;
+    transaction.feePayer = fromWallet.publicKey;
+    const signature = await sendAndConfirmTransaction(connection, transaction, [
+      fromWallet,
+    ]);
+    console.log(
+      `Success! https://explorer.solana.com/tx/${signature}?cluster=devnet`
+    );
+  } catch (error) {
+    console.log(`Oops, something went wrong: ${error}`);
+  }
+})();
+//First transfer - https://explorer.solana.com/tx/4f2o8KdwozZ5MzvLsDdxv2MQ3sgVqhTgTruGN27KsRmYRmuXxoKQgXEFpPQtamMXPMc94R5H5Da4Zz95XiWyZuyh?cluster=devnet
+//Full transfer - https://explorer.solana.com/tx/4trKiZEqrsGxVCSnqrzHgeMXEvBVwa4qxvWyMVD8XP1qUJumzqBP1Q2ntJtdEE4EyFBR6vJNvarHLQMtAsUUvbe5?cluster=devnet
